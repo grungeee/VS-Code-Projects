8d52ed83aa026d556763f35469dae334758915b9
grungeee <sharapanikita@gmail.com>
2022-06-20 00:00:04 +0200

    gooo

--- HTML-CSS Course/09-Omnifood-Optimizations/js/script.js
+++ HTML-CSS Course/09-Omnifood-Optimizations/js/script.js
@@ -25,10 +25,57 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+///////////////////////////////////////////////////////////
+// Smoot scrolling animation
+const allLinks = document.querySelectorAll("a:link");
 
-// btnMobileNav.addEventlistener('click', function() {
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
 
-// });
+    //scrolls back to the top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    //scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+///////////////////////////////////////////////////////////
+//Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //  In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
@@ -51,6 +98,10 @@ function checkFlexGap() {
 checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+/******************************/
+/** Fixing Safari flexobox gap **/
+/******************************/
 
 /*
 .no-flexbox-gap .main-nav-list li:not(:last-child) {
