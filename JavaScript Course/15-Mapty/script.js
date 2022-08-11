d2407b5d369a4d7ccd97271d0924f8fc10152ca6
WITRJBZTRMURAD <WITRJBZTRMURAD@reaktiv.local>
2022-08-11 13:47:38 +0200

    finally I found courage to continue this chapter

--- JavaScript Course/15-Mapty/script.js
+++ JavaScript Course/15-Mapty/script.js
@@ -1,7 +1,71 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+//* Displaying a Map Using Leaflet Library
+
+let map, mapEvent;
+
+class Wourkout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; //: [lat, lng]
+    this.distance = distance; //: in km
+    this.duration = duration; //: in min
+  }
+}
+
+//- Children
+
+//. Running
+class Running extends Wourkout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    //: min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+//. Cycling
+class Cycling extends Wourkout {
+  constructor(coords, distance, duration, eleveationGain) {
+    super(coords, distance, duration);
+    this.eleveationGain = eleveationGain;
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+//: test if the objects are working
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
+
+//* Application Architatcutre
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -11,19 +75,23 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-//* Displaying a Map Using Leaflet Library
-
-let map, mapEvent;
-
 class App {
-  constructor() {}
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition;
+
+    //- Form Events
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
   _getPosition() {
     //> Takes 2 callback functions -> 1: on success, 2: error
     //. Success function
     if (navigator.geolocation)
       navigator.geolocation.getCurrentPosition(
-        this._loadMap,
+        this._loadMap.bind(this),
 
         //. Error funtion
         function () {
@@ -45,63 +113,76 @@ class App {
     //- "Leaflet" library integration
     //> 1: parameter is coordinates array, 2: Zoom level
     // const map = L.map('map').setView([51.505, -0.09], 13);
-    map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, 13);
 
     //> it is possible to use another map f.a.: Goggle maps
     //> also you can change the appearance of the map tiles (map squares)
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
     //- Handling clicks on map
     //> leaflet object
-    map.on('click', function (mapE) {
-      mapEvent = mapE;
-      form.classList.remove('hidden');
-      inputDistance.focus();
-    });
+    this.#map.on('click', this._showForm.bind(this));
   }
 
-  _showForm() {}
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
 
-  _toggleElevationField() {}
+  _toggleElevationField(e) {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-  _newWorkout() {}
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Get data from form
+
+    const type = inputType.value;
+    const distnace = inputDistance.value;
+    const cadence = inputCadence.value;
+    const elevation = inputElevation.value;
+
+    //Check if datis valid
+
+    // If workout runngin, create running object
+
+    // If workout cycling, create cycling object
+
+    // Add new object tor workout array
+
+    // Render workout on map as array
+
+    //. Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    //. Display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    console.log(this);
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          maxHeight: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
 
-//- Form Events
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  //. Clear input fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
-
-  //. Display marker
-  console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
-
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        maxHeight: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('Workout')
-    .openPopup();
-});
-
-inputType.addEventListener('change', function (e) {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+const app = new App();
+app._getPosition();
