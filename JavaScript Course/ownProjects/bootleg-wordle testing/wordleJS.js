6107ee4bac5f70cdde8af82ffe5e662b4f4f0426
grungeee <sharapanikita@gmail.com>
2022-08-28 23:19:59 +0200

    i am really close to find a solution

--- JavaScript Course/ownProjects/bootleg-wordle testing/wordleJS.js
+++ JavaScript Course/ownProjects/bootleg-wordle testing/wordleJS.js
@@ -50,6 +50,7 @@ const testDB = [
   'hello',
   'jotty',
   'treat',
+  //>
 ];
 // console.log(testDB);
 
--- JavaScript Course/ownProjects/playground/script.js
+++ JavaScript Course/ownProjects/playground/script.js
@@ -41,11 +41,73 @@
 //   return array.filter(item => item === what).length;
 // }
 
-//>
+//&
 
+//: |=========================|
+//: |======/ Functions /======|
+function countInArray(array, what) {
+  array.filter(item => item === what).length;
+}
+//: |=========================|
+//: |=======/ Colors /========|
 const green = '[ 🟩 ]';
 const yellow = '[ 🟨 ]';
-const red = '[ 🟥 ]';
+const none = '[ 🔳 ]';
+//: |=========================|
+const wordle = 'jorty'; //: previous: 'jotty'
+const guess = 'trert';
+//. cl logs
+const wordleSplit = wordle.split('');
+const guessSplit = guess.split('');
+//: |=========================|
+console.log(wordleSplit);
+console.log(guessSplit);
+//: |=========================|
+//: |====/ Adding Colors /====|
 
-const wordle = 'jotty';
-const guess = 'treat';
+//> --------| Arrays |--------
+//? maybe an array with all duplicates and no '' stings
+const gArr = [];
+const wArr = [];
+for (g of guess) {
+  wordle.includes(g) && gArr.push(g);
+}
+for (w of wordle) {
+  guess.includes(w) && wArr.push(w);
+}
+
+console.log('g in wordle: ', gArr, 'w in guess: ', wArr);
+// console.log('shallow copy', gArr.slice());
+
+wordleSplit.forEach((w, index) => {
+  //> ------| Variables |-------
+  const g = guess[index];
+  const wordleIncludesG = wordle.includes(g);
+  const guessIncludesG = guess.includes(w);
+  const gInWordle = wordle.includes(g) ? g : '-';
+  const wInGuess = guess.includes(w) ? w : '-';
+
+  //> ----| Pushing to Arr |----
+  // gArr.push(wordle.includes(g) ? g : guess.includes(w) ? w : '');
+
+  //> ------| cl logs |-------
+  console
+    .log
+    // '-------------'
+    // gInWordle,
+    // wInGuess /* gInWordle === wInGuess */,
+    // gArr
+    ();
+  //> ------------------------
+  if (w === g) {
+    console.log(w.toUpperCase(), green, g.toUpperCase());
+  } else if (wordleIncludesG && gArr[index] === wArr[index]) {
+    console.log(w.toUpperCase(), yellow, g.toUpperCase());
+  } else {
+    console.log(w.toUpperCase(), none, g.toUpperCase());
+  }
+  //> w === a = value if wordleSplit.contains(value) !== guessSplit.contains(value) ? remove last yellow
+  //> add as many yellow as there is value in wordleSplit
+  //> add as many yellow if wordleSplit.contains(value).length === guessSplit.contains(value).length
+});
+//: |=========================|
